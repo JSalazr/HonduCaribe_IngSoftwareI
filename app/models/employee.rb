31d267b34571e86613d77e73f111fda73247feb3
vagrant <vagrant@precise32.(none)>
2017-05-24 20:23:31 +0000

    Fixed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -9,10 +9,10 @@ class Employee < ActiveRecord::Base
 	:path => "/assets/photos/:id/:style/:basename.:extension",
 	:s3_host_name => "s3-us-west-2.amazonaws.com",
 	s3_credentials: {
-    bucket: 'honducariberrhh',
-    access_key_id: 'AKIAID2VBHCVFGZAJ4SQ',
-    secret_access_key: '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',
-    s3_region: 'us-west-2',
+		bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 
 
