4f133e6406691423dfae1f4d8c41dd5c0ae386cd
vagrant <vagrant@precise32.(none)>
2017-05-24 18:50:25 +0000

    Images hopefully fixed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,12 +4,17 @@ class Employee < ActiveRecord::Base
 	validates_numericality_of :emergency_contact_number, greater_than:0
 	has_attached_file :image,
 	:storage => :s3,
-	:bucket => 'honducariberrhh',
-	:s3_permissions => :public,
-	:url => ":s3_domain_url",
+	:s3_permissions => 'public-read-write',
+	:url => "s3-us-west-2.amazonaws.com/honducariberrhh",
 	:path => "/assets/photos/:id/:style/:basename.:extension",
-	:s3_host_name => "s3-website-us-west-2.amazonaws.com",
-	:s3_credentials => :"#{Rails.root}/config/s3.yml"
+	:s3_host_name => "s3-us-west-2.amazonaws.com",
+	:s3_credentials => {
+		bucket: ENV.fetch('S3_BUCKET_NAME'),
+		access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+		secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+		s3_region: ENV.fetch('AWS_REGION')
+  }
+
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
