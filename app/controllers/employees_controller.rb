06ceeb594c842cc14d807ae545fb3d6c3e0ca6b1
vagrant <vagrant@precise32.(none)>
2017-05-25 22:00:49 +0000

    Let's hope

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,12 @@
 class EmployeesController < ApplicationController
 
+  def hours_show
+    respond_to do |format|
+      format.html
+      format.js { render "hours_show", locals: {employee: Employee.find(params[:employee_id]), from: params[:report][:from], to: params[:report][:to]}}
+    end
+  end
+
   def hours
     @employees = Employee.where(employee_status: true)
 
